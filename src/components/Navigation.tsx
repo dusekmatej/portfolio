af372a4f3afaa9936c6a5fe5eebd451d7633aa6f
Mates Dušek <mat.dusek@gmail.com>
2025-11-28 20:51:16 +0100

    Added some frontend changes and started work on articles section

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,17 +4,17 @@ const Navigation = () => {
     return (
         <div className="flex gap-10 justify-center text-2xl pt-5">
             
-            <div className="relative glow-group">
+            <div className="relative glow-group move-effect">
                 <Link to="/">Home</Link>
                 <div className="circle-glow"></div>
             </div>
 
-            <div className="relative glow-group">
+            <div className="relative glow-group move-effect">
                 <Link to="/projects">Projects</Link>
                 <div className="circle-glow"></div>
             </div>
             
-            <div className="relative glow-group">
+            <div className="relative glow-group move-effect">
                 <Link to="/articles">Articles</Link>
                 <div className="circle-glow"></div>
             </div>
--- src/routes/articles.tsx
+++ src/routes/articles.tsx
@@ -5,5 +5,12 @@ export const Route = createFileRoute('/articles')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/articles"!</div>
+  return (
+    <div className="flex justify-center">
+        <ul className="absolute top-40 text-3xl">
+          <li className="m-5"><a className='' href="#">How to fake git commits.</a></li>
+          <li className="m-5"><a className='' href="#">Article</a></li>
+        </ul>
+    </div>
+  );
 }
