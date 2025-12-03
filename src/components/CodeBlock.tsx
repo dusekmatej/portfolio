81329563d7977b85c514a2771ebc91963b037bd0
Mates Dušek <mat.dusek@gmail.com>
2025-12-03 21:59:44 +0100

    Optimized code, broke little bit responzivity but overall the responzivity is better.

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -9,14 +9,14 @@ const CodeBlock = () => {
             </div>
 
                 <div className="codeblock-body">
-                    <p>
+                    <p className="ml-[2vw] text-left">
                         {DisplayTextAnimation("Hi, my names is Matěj Dušek, I am a currently studying IT with main focus on programming. Outside of school, I work on my personal projects, some of them are visible on my GitHub profile for example I've made message encryption program in C#, console-based mini RPG game in C# or this website, which is made with react. My main focus is on C# backend development, but I can work in Python, Web Technologies and I have basics of JavaScript. I'm commited to learning new languages and improving my skills in programming.", 1)}
                     </p>
                     
 
 
 
-                    <div className="codeblock-line-numbers">
+                    <div className="absolute left-3 top-6 mt-[4.5vh] ml-[1vw] leading-[170%] codeblock-lines">
                             1<br/>
                             2<br/>
                             3<br/>
